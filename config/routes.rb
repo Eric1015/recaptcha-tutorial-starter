50d7758c17a780d13a05691c55b0986429637638
love1 <love1dbsb77@hotmail.com>
2018-07-15 19:38:48 -0700

    Changed the development database to sqlite3 basis

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'users#new'
-  resources :users
+  resources :users, only: [:index, :new]
   get '/thanks', to: 'users#thanks'
 end
